d08dba72200a7820e32430ada1900f88251e6c87
Federico <baldinifederico96@gmail.com>
2020-03-02 00:35:29 +0100

    renamed properties

--- interpreter/src/modules/Language.js
+++ interpreter/src/modules/Language.js
@@ -2,6 +2,8 @@ const Language = {}
 
 const SCOPE = {}
 
+SCOPE['alert'] = window.alert
+
 Language.define = function define(property) {
   SCOPE[property] = undefined
 }
@@ -10,40 +12,40 @@ Language.assign = function assign(property, value) {
   SCOPE[property] = value
 }
 
-Language.plus = function plus(operand1, operand2) {
-  return operand1 + operand2
+Language.plus = function plus(firstOperand, secondOperand) {
+  return firstOperand + secondOperand
 }
 
-Language.minus = function minus(operand1, operand2) {
-  return operand1 - operand2
+Language.minus = function minus(firstOperand, secondOperand) {
+  return firstOperand - secondOperand
 }
 
-Language.multiply = function multiply(operand1, operand2) {
-  return operand1 * operand2
+Language.multiply = function multiply(firstOperand, secondOperand) {
+  return firstOperand * secondOperand
 }
 
-Language.divide = function divide(operand1, operand2) {
-  return operand1 / operand2
+Language.divide = function divide(firstOperand, secondOperand) {
+  return firstOperand / secondOperand
 }
 
-Language.equal = function equal(operand1, operand2) {
-  return operand1 === operand2
+Language.equal = function equal(firstOperand, secondOperand) {
+  return firstOperand === secondOperand
 }
 
-Language.greaterThan = function greaterThan(operand1, operand2) {
-  return operand1 > operand2
+Language.greaterThan = function greaterThan(firstOperand, secondOperand) {
+  return firstOperand > secondOperand
 }
 
-Language.lessThan = function lessThan(operand1, operand2) {
-  return operand1 < operand2
+Language.lessThan = function lessThan(firstOperand, secondOperand) {
+  return firstOperand < secondOperand
 }
 
-Language.greaterOrEqualThan = function greaterOrEqualThan(operand1, operand2) {
-  return Language.greaterThan(operand1, operand2) || Language.equal(operand1, operand2)
+Language.greaterOrEqualThan = function greaterOrEqualThan(firstOperand, secondOperand) {
+  return Language.greaterThan(firstOperand, secondOperand) || Language.equal(firstOperand, secondOperand)
 }
 
-Language.lessOrEqualThan = function lessOrEqualThan(operand1, operand2) {
-  return Language.lessThan(operand1, operand2) || Language.equal(operand1, operand2)
+Language.lessOrEqualThan = function lessOrEqualThan(firstOperand, secondOperand) {
+  return Language.lessThan(firstOperand, secondOperand) || Language.equal(firstOperand, secondOperand)
 }
 
 Language.if = function ifCondition(condition, whenTrue, whenFalse) {}
@@ -52,12 +54,12 @@ Language.forEach = function forEach(items, callback) {
   items.forEach(item => callback(item))
 }
 
-Language.execute = function execute(fn) {}
+Language.execute = function execute(fn) {
+  fn()
+}
 
 Language.call = function call(fn) {}
 
-Language.evaluate = function evaluate(expression) {
-  console.log(expression)
-}
+Language.evaluate = function evaluate(expressions) {}
 
 export default Language
