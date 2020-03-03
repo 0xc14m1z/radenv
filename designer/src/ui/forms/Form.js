77f397e7a5544499d8593be50e1f88f8a0a7466b
Luca Ramundo <0xc14m1z@gmail.com>
2020-03-03 09:47:10 +0100

    Add TextField component

--- designer/src/ui/forms/Form.js
+++ designer/src/ui/forms/Form.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Formik } from 'formik'
 import { Form as UIForm } from 'semantic-ui-react'
 
+import { TextField } from './TextField'
+
 const Form = ({ initialValues, schema, onSubmit, children, ...props }) => (
   <Formik
     initialValues={ initialValues }
@@ -25,5 +27,7 @@ const Form = ({ initialValues, schema, onSubmit, children, ...props }) => (
     }
   </Formik>
 )
+
+Form.TextField = TextField
 
 export { Form }
